c6d1deecea4a184cbd8bf9d3bb9b5c83b1a67010
Rafa Lopes <rafalopessecond@gmail.com>
2024-03-11 23:10:07 +0000

    Rafa Lopes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#4eb6ac",
+				secondary: "#e5ffff",
+				tertiary: "#1d313a",
+			},
+		},
+	},
+	plugins: [],
+};
